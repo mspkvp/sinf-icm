fb067607f0ce85a7392a2b2303325f2761ea97bc
Mike Pinto <msp4ever@gmail.com>
2014-11-18 20:58:22 +0000

    login template (starter template) and basic controller functionality

--- webapp/app/scripts/controllers/login.controller.js
+++ webapp/app/scripts/controllers/login.controller.js
@@ -1,6 +1,34 @@
 'use strict';
 
 angular.module('icmApp')
-  .controller('LoginCtrl', ['$scope', function ($scope) {
+  .controller('LoginCtrl', ['$scope', '$location', function ($scope, $location) {
 
+  	$scope.login_data = {
+  		username: '',
+  		password: ''
+  	}
+
+  	$scope.login = function login () {
+  		$location.path('/');
+  	};
+
+  	this.showPassword = function showPassword() {
+    
+	    var key_attr = $('#key').attr('type');
+	    
+	    if(key_attr != 'text') {
+	        
+	        $('.checkbox').addClass('show');
+	        $('#key').attr('type', 'text');
+	        
+	    } else {
+	        
+	        $('.checkbox').removeClass('show');
+	        $('#key').attr('type', 'password');
+	        
+	    }
+	    
+	};
+
+	
   }]);
