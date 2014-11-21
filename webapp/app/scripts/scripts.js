af028a43d5d513df80a20888a9cf5be89f671126
Mike Pinto <msp4ever@gmail.com>
2014-11-21 15:02:38 +0000

    api endpoints: a bit more

--- webapp/app/scripts/scripts.js
+++ webapp/app/scripts/scripts.js
@@ -1,8 +1,23 @@
-var endpointsAPI = {
-	login: {
-
-	},
-	register : {
-
-	}
-};
+var	endpointsAPI = {
+		serverUrl: '',
+		login: {
+			method: 'POST',
+			url: this.serverUrl + ''
+		},
+		register : {
+			method: 'POST',
+			url: this.serverUrl + ''
+		},
+		clients : { 
+			method: 'GET',
+			url: this.serverUrl + ''
+		},
+		suppliers : { 
+			method: 'GET',
+			url: this.serverUrl + ''
+		},
+		companies : {
+			method: 'GET',
+			url: this.serverUrl + ''
+		}
+	};
