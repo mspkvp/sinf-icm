12ac9fb1900d9030f8f3cba4fb6c312da1faaf03
Mike Pinto <msp.univ.por@gmail.com>
2014-12-08 22:37:06 +0000

    api endpoints finished

--- webapp/app/scripts/scripts.js
+++ webapp/app/scripts/scripts.js
@@ -1,23 +1,185 @@
-var serverUrl = 'http://localhost/'; // actualizar com o necessário
+var serverUrl = 'http://localhost/FirstREST/api/'; // actualizar com o necessário
+// A empresa tem de ir sempre nos headers 'empresa={{idEmpresa}}'
+// byID significa que o id faz parte do url, é um simples append da variável id ao URL
 var	endpointsAPI = {
-		login: {
-			method: 'POST',
-			url: serverUrl + 'login'
+	clients : { 
+		default: {
+			get: {
+				method: 'GET',
+				url: serverUrl + 'Clientes'
+			},
+			post: {
+				method: 'POST',
+				url: serverUrl + 'Clientes'
+			}
 		},
-		register : {
-			method: 'POST',
-			url: serverUrl + ''
-		},
-		clients : { 
-			method: 'GET',
-			url: serverUrl + ''
-		},
-		suppliers : { 
-			method: 'GET',
-			url: serverUrl + ''
-		},
-		companies : {
-			method: 'GET',
-			url: serverUrl + ''
+		byID: {
+			get: {
+				method: 'GET',
+				url: serverUrl + 'Clientes/'
+			},
+			put: {
+				method: 'PUT',
+				url: serverUrl + 'Clientes/'
+			},
+			delete: {
+				method: 'DELETE',
+				url: serverUrl + 'Clientes/'
+			}
 		}
-	};
+	},
+	suppliers : {
+		default: {
+			get: {
+				method: 'GET',
+				url: serverUrl + 'Fornecedores'
+			},
+			post: {
+				method: 'POST',
+				url: serverUrl + 'Fornecedores'
+			}
+		},
+		byID: {
+			get: {
+				method: 'GET',
+				url: serverUrl + 'Fornecedores/'
+			},
+			put: {
+				method: 'PUT',
+				url: serverUrl + 'Fornecedores/'
+			},
+			delete: {
+				method: 'DELETE',
+				url: serverUrl + 'Fornecedores/'
+			}
+		}
+	},
+	articles: {
+		default: {
+			get: {
+				method: 'GET',
+				url: serverUrl + 'Artigos'
+			},
+			post: {
+				method: 'POST',
+				url: serverUrl + 'Artigos'
+			}
+		},
+		byID: {
+			get: {
+				method: 'GET',
+				url: serverUrl + 'Artigos/'
+			},
+			put: {
+				method: 'PUT',
+				url: serverUrl + 'Artigos/'
+			},
+			delete: {
+				method: 'DELETE',
+				url: serverUrl + 'Artigos/'
+			}
+		}
+	},
+	values: { // este n precisa da empresa no header
+		default: {
+			get: {
+				method: 'GET',
+				url: serverUrl + 'Values'
+			},
+			post: {
+				method: 'POST',
+				url: serverUrl + 'Values'
+			}
+		},
+		byID: {
+			get: {
+				method: 'GET',
+				url: serverUrl + 'Values/'
+			},
+			put: {
+				method: 'PUT',
+				url: serverUrl + 'Values/'
+			},
+			delete: {
+				method: 'DELETE',
+				url: serverUrl + 'Values/'
+			}
+		}
+	},
+	doc : {
+		order: {
+			to.supplier : {
+				default: {
+					get: {
+						method: 'GET',
+						url: serverUrl + 'DocEncomendaAFornecedor'
+					},
+					post: {
+						method: 'POST',
+						url: serverUrl + 'DocEncomendaAFornecedor'
+					}
+				},
+				byID: {
+					get: {
+						method: 'GET',
+						url: serverUrl + 'DocEncomendaAFornecedor/'
+					}
+				}
+			},
+			from.client : {
+				default: {
+					get: {
+						method: 'GET',
+						url: serverUrl + 'DocEncomendaDeCliente'
+					},
+					post: {
+						method: 'POST',
+						url: serverUrl + 'DocEncomendaDeCliente'
+					}
+				},
+				byID: {
+					get: {
+						method: 'GET',
+						url: serverUrl + 'DocEncomendaDeCliente/'
+					}
+				}
+			}
+		},
+		invoice : {
+			default: {
+				get: {
+					method: 'GET',
+					url: serverUrl + 'DocFatura'
+				},
+				post: {
+					method: 'POST',
+					url: serverUrl + 'DocFatura'
+				}
+			},
+			byID: {
+				get: {
+					method: 'GET',
+					url: serverUrl + 'DocFatura/'
+				}
+			}
+		},
+		v.invoice : {
+			default: {
+				get: {
+					method: 'GET',
+					url: serverUrl + 'DocVFatura'
+				},
+				post: {
+					method: 'POST',
+					url: serverUrl + 'DocVFatura'
+				}
+			},
+			byID: {
+				get: {
+					method: 'GET',
+					url: serverUrl + 'DocVFatura/'
+				}
+			}
+		}
+	}
+};
