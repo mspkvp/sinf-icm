bf34fecf9810932caacfc88a731793faa03bfc4c
João Carlos Santos <joaofloressantos@gmail.com>
2014-12-18 17:12:36 +0000

    Bug

--- webapp/app/scripts/controllers/profile.controller.js
+++ webapp/app/scripts/controllers/profile.controller.js
@@ -4,7 +4,7 @@ angular.module('icmApp')
   .controller('ProfileCtrl', ['$scope', 'NavigationService', 'UserService',  function controller($scope, $nav, $userS) {
     if (!$userS.getLoginStatus()) {
       alert("Please login first!");
-      $nav.setRedirection('/login');
+      $nav.setRedirection('login');
       $nav.go('login');
       return;
     }
